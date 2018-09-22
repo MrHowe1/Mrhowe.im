8aef762733a85868c52011e32cd98496ea514971
Jamie Howe <jamiehowe@Jamies-MacBook-Air.local>
2018-09-22 19:57:13 +0100

    Initial Commit

---
+++ accountancy.php
@@ -0,0 +1,35 @@
+<html lang="en">
+  <head>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>MrHowe.im</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/main.css" rel="stylesheet">
+  </head>
+	
+	<body>
+		<?php include_once("includes/nav.php") ?>
+		
+<main role="main" class="container">
+
+	<div class="row">
+		<div class="home col-md-4 offset-md-4 text-center">
+			<h1><u>Accountancy</u></h1>
+		</div>
+	</div>
+	
+	<div class="row">
+	  	<div class="home col-md-4 offset-md-4 text-center">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+</main>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>MrHowe.im</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/main.css" rel="stylesheet">
+  </head>
+
+  <body>
+<?php include_once("includes/nav.php") ?>
+
+    <main role="main" class="container">
+		 <div class="row">
+      		<div class="home col-md-6 offset-md-3 text-center">
+        		<h1>MrHowe.im</h1>
+        		<p class="lead">Welcome to MrHowe.im 2</p>
+		  		<img src="/images/meme-code.jpg" alt="meme" width="300">
+			</div>
+            <div class="home col-md-6 offset-md-3 text-center">
+                <p>This website is still in progress, if there is anything you would like to see, please don't hesitate to get in touch</p>
+            </div>
+		</div>
+	  </main>
+	  
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
