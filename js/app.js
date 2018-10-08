ba61b413a7530387d83dd911de6ed0e69ac7c699
Jamie Howe <jamiehowe@Jamies-MacBook-Air.local>
2018-10-08 21:42:12 +0100

    Disabled ‘app.js’

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-alert("testing JQuery")
-var x = 5;
+//alert("testing JQuery")
+//var x = 5;
 //var y = 7;
-alert(x + 7)
+//alert(x + 7)
 
 
-$(document).ready(function() {
-    console.log( "ready!" );
-   alert($('body').html())
+//$(document).ready(function() {
+//    console.log( "ready!" );
+//   alert($('body').html())
 
-});
+//});
