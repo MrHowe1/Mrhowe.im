7ff043602dd46a8f66d56ebf477e4144756afc7b
Jamie Howe <jamiehowe@Jamies-MacBook-Air.local>
2018-10-23 18:13:49 +0100

    Tidied up code to ensure clarity of format and began adding in content to ‘about.php’

--- about.php
+++ about.php
@@ -30,7 +30,7 @@
 	
 	<div class="row">
 	  	<div class="home col-md-4 offset-md-4 text-center">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p> Jamie Howe is a bartending and technology enthusiast from the Isle of Man. Spent 2 years with the Isle Of Man Army Cadet Force learning discipline and key life skills whilst maintaining a part time job at a local cleaning firm. </p>
 		</div>
 	</div>
 </main>
--- index.php
+++ index.php
@@ -21,40 +21,38 @@
   <body>
 <?php include_once("includes/nav.php") ?>
 
-    <main role="main" class="container">
+<main role="main" class="container">
 		 <div class="row">
       		<div class="home col-md-6 offset-md-3 text-center">
         		<h1>MrHowe.im</h1>
         		<p class="lead">Welcome to MrHowe.im</p>
 		  		<img src="/images/meme-code.jpg" alt="meme" width="300">
 		  			<p>This website is still in progress, if there is anything you would like to see, please don't hesitate to get in touch</p>
-		  			<p></p>
             </div>
            </div>
 
 
-<div class="row">
-  <div class="col-sm-4 offset-2">
-    <div class="card text-white bg-black">
-      <div class="card-body">
-        <h5 class="card-title">About the maker</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
-        <a href="/about.php" class="btn btn-primary">Go to Page</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card text-white bg-black">
-      <div class="card-body">
-        <h5 class="card-title">Accounting</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
-        <a href="/accountancy.php" class="btn btn-primary">Go to Page</a>
-      </div>
-    </div>
-  </div>
-</div>
-	
-	  </main>
+	<div class="row">
+		<div class="col-sm-4 offset-2">
+			<div class="card text-white bg-black">
+			<div class="card-body">
+				<h5 class="card-title">About the maker</h5>
+				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+				<a href="/about.php" class="btn btn-primary">Go to Page</a>
+      		</div>
+    		</div>
+  		</div>
+  		<div class="col-sm-4">
+    		<div class="card text-white bg-black">
+      			<div class="card-body">
+        			<h5 class="card-title">Accounting</h5>
+					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+					<a href="/accountancy.php" class="btn btn-primary">Go to Page</a>
+      			</div>
+    		</div>
+  		</div>
+	</div>
+</main>
 	 
     <!-- Bootstrap core JavaScript
     ================================================== -->
